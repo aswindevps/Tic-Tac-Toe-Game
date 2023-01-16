da0d43996564af4381cdf1bacb85eae9822524a1
Aswin Dev P.S <aswindevps@gmail.com>
2023-01-16 19:47:21 +0100

    Add implementation for createNewGame method.
    Run testcase and verify.

--- tictactoe-server/src/main/java/com/game/tictactoe/service/impl/TicTacToeServiceImpl.java
+++ tictactoe-server/src/main/java/com/game/tictactoe/service/impl/TicTacToeServiceImpl.java
@@ -1,11 +1,17 @@
 package com.game.tictactoe.service.impl;
 
+import com.game.tictactoe.constant.TicTacToeConstant;
+import com.game.tictactoe.entity.Board;
 import com.game.tictactoe.entity.Game;
+import com.game.tictactoe.entity.Player;
 import com.game.tictactoe.repository.BoardRepository;
 import com.game.tictactoe.repository.GameRepository;
 import com.game.tictactoe.repository.PlayerRepository;
 import com.game.tictactoe.service.TicTacToeService;
 import org.springframework.stereotype.Service;
+
+import java.util.ArrayList;
+import java.util.List;
 
 @Service
 public class TicTacToeServiceImpl implements TicTacToeService {
@@ -22,6 +28,29 @@ public class TicTacToeServiceImpl implements TicTacToeService {
 
     @Override
     public Game createNewGame(Integer size) {
-        return null;
+        Board board = new Board(size);
+        board = boardRepository.save(board);
+
+        Game game = new Game();
+        game.setBoard(board);
+
+        Player player1 = new Player();
+        player1.setName("Player 1");
+        player1.setSymbol(TicTacToeConstant.PLAYER_1);
+        player1 = playerRepository.save(player1);
+
+        Player player2 = new Player();
+        player2.setName("Player 2");
+        player2.setSymbol(TicTacToeConstant.PLAYER_2);
+        player2 = playerRepository.save(player2);
+
+        List<Player> players = new ArrayList<>();
+        players.add(player1);
+        players.add(player2);
+
+        game.setPlayers(players);
+        game.setCurrentPlayer(player1);
+        game.setStatus(TicTacToeConstant.GAME_IN_PROGRESS);
+        return gameRepository.save(game);
     }
 }
